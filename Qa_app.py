347fe5e96e90372c1595dee7954ea1c579c8ecb0
sharanya39 <shanvv1321@gmail.com>
2025-03-07 16:23:53 -0600

    pdf document added

--- Qa_app.py
+++ Qa_app.py
@@ -128,4 +128,6 @@ def answer_question():
 # START SERVER
 # -------------------
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port
+    app.run(host="0.0.0.0", port=port, debug=True)
+    #app.run(host="0.0.0.0", port=5000, debug=True)
